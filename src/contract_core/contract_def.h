f1a67197184bff140fc51fda90738aeb948cd554
Soufiane Benbah <soufiane.benbah.work@gmail.com>
2026-03-26 17:46:11 +0100

    Add QubicSolanaBridge smart contract (QSB, index 27)

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -278,11 +278,11 @@
 #undef CONTRACT_STATE_TYPE
 #undef CONTRACT_STATE2_TYPE
 
-#define ESCROW_CONTRACT_INDEX 27
-#define CONTRACT_INDEX ESCROW_CONTRACT_INDEX
-#define CONTRACT_STATE_TYPE ESCROW
-#define CONTRACT_STATE2_TYPE ESCROW2
-#include "contracts/Escrow.h"
+#define QSB_CONTRACT_INDEX 27
+#define CONTRACT_INDEX QSB_CONTRACT_INDEX
+#define CONTRACT_STATE_TYPE QSB
+#define CONTRACT_STATE2_TYPE QSB2
+#include "contracts/QubicSolanaBridge.h"
 
 // new contracts should be added above this line
 
@@ -397,7 +397,6 @@ constexpr struct ContractDescription
 	{"PULSE", 204, 10000, sizeof(PULSE::StateData)}, // proposal in epoch 202, IPO in 203, construction and first use in 204
     {"VOTTUN", 206, 10000, sizeof(VOTTUNBRIDGE::StateData)}, // proposal in epoch 204, IPO in 205, construction and first use in 206
     {"QUSINO", 208, 10000, sizeof(QUSINO::StateData)}, // proposal in epoch 206, IPO in 207, construction and first use in 208
-    {"ESCROW", 210, 10000, sizeof(ESCROW::StateData)}, // proposal in epoch 208, IPO in 209, construction and first use in 210
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA::StateData)},
@@ -520,7 +519,6 @@ static void initializeContracts()
 	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(PULSE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(VOTTUNBRIDGE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QUSINO);
-    REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(ESCROW);
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
