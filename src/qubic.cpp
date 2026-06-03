429bbb802d8e42990864f72a5c42e45a6e530dd8
feiyu <lvp123321123321@gmail.com>
2026-06-03 19:13:39 +0700

    disable NO_GGWP

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -64,7 +64,7 @@
 
 // #define INCLUDE_CONTRACT_TEST_EXAMPLES
 
-// #define NO_GGWP
+#define NO_GGWP
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
